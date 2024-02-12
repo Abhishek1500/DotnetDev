6e633cf07664928ce6709d2327a740f8e2de8eb8
Abhishek1500 <abhishekparmar1500@gmail.com>
2024-02-13 01:13:28 +0530

    frontendwork learn angular bro from somewhere else

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -10,28 +12,29 @@ import { User } from '../_models/user';
 })
 export class NavComponent {
   model: any={}
-  constructor(public accountService:AccountService ){}
+  constructor(public accountService:AccountService,private router:Router,
+    private toastr:ToastrService){}
 
   ngOnInit():void{
     
   }
 
-
-
-
-
   login(){
 
     this.accountService.login(this.model).subscribe({
-      next: response=>{
-        console.log(response);
+      next: ()=>{
+        this.router.navigateByUrl('/members')
       },
-      error: err=>console.log(err)
+      error: err=>{
+        this.toastr.error(err.error);
+        console.log(err);
+      }
     })
 
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
